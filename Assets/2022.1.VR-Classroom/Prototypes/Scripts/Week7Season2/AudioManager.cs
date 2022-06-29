9170827faf410de6fe7ffe2c98def8200c627997
paulwhite93 <pwhite93@uw.edu>
2022-06-29 13:33:11 -0700

    fixed audio manager

--- Assets/2022.1.VR-Classroom/Prototypes/Scripts/Week7Season2/AudioManager.cs
+++ Assets/2022.1.VR-Classroom/Prototypes/Scripts/Week7Season2/AudioManager.cs
@@ -31,8 +31,7 @@ public class AudioManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {        
-        VoiceUI = (GameObject)Resources.Load("MyPrefabs/VoIP UI");
-        PrefabVoIP_UI = (GameObject)Resources.Load("MyPrefabs/VoIP UI");
+        PrefabVoIP_UI = (GameObject)Resources.Load("MyPrefabs/VoIP_UI");
     }
 
     // Update is called once per frame
