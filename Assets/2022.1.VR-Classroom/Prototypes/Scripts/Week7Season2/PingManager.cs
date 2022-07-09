5c37628359a9cbe4c5b8142ba4c270ca89b2fc93
River <riverehill@gmail.com>
2022-07-08 18:34:31 -0700

    Send ping to connected users

--- Assets/2022.1.VR-Classroom/Prototypes/Scripts/Week7Season2/PingManager.cs
+++ Assets/2022.1.VR-Classroom/Prototypes/Scripts/Week7Season2/PingManager.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ASL;
 
 public class PingManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float waitTime = 5;
+    ASLObject m_ASLObject;
+    float[] id = new float[2];
+
     void Start()
     {
-        
+        m_ASLObject = GetComponent<ASLObject>();
+        m_ASLObject._LocallySetFloatCallback(FloatReceive);
+
+        if (GameManager.AmTeacher)
+            StartCoroutine(SendPing());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator SendPing()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            id[0] = 100;
+            m_ASLObject.SendAndSetClaim(() => {
+                m_ASLObject.SendFloatArray(id);
+            });
+        }
+    }
+
+    void FloatReceive(string _id, float[] _f)
+    {
+        if((int)_f[0] == 100)
+        {
+            Debug.Log("Recieved Teachers Ping");
+        }
     }
 }
