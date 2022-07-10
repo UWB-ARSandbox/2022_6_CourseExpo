8b3f1c2c84ad7af9b09f1df6b4e231d685d9635a
River <riverehill@gmail.com>
2022-07-09 21:38:51 -0700

    Rewrite ping system

--- Assets/2022.1.VR-Classroom/Prototypes/Scripts/Week7Season2/PingManager.cs
+++ Assets/2022.1.VR-Classroom/Prototypes/Scripts/Week7Season2/PingManager.cs
@@ -11,20 +11,29 @@ public class PingManager : MonoBehaviour
     float[] myFloats = new float[2];
     public Dictionary<int, string> playerList;
 
+    List<int> originalPlayers = new List<int>();
     List<int> connectedPlayers = new List<int>();
     private const float PINGREQUEST = 1000;
     private const float PINGRESPONSE = 1001;
 
-
     void Start()
     {
         m_ASLObject = GetComponent<ASLObject>();
-        m_ASLObject._LocallySetFloatCallback(FloatReceive);
+        // m_ASLObject._LocallySetFloatCallback(FloatReceive);
         
         // Create player list and start the ping coroutine
         if (GameManager.AmTeacher)
         {
-            playerList = GameLiftManager.GetInstance().m_Players;
+
+            // foreach (int playerID in playerList.Keys)
+            // {
+            //     if (playerID != 1)
+            //     {
+            //         originalPlayers.Add(playerID);
+            //     }
+            // }
+
+            // playerList = GameLiftManager.GetInstance().m_Players;
             StartCoroutine(SendPing());
         }
     }
@@ -34,60 +43,65 @@ public class PingManager : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(waitTime);
-            // send an individual ping to each connected player
-            myFloats[0] = PINGREQUEST;
-            foreach (int playerID in playerList.Keys)
+            // // send an individual ping to each connected player
+            // myFloats[0] = PINGREQUEST;
+            // foreach (int playerID in originalPlayers)
+            // {
+            //     myFloats[1] = playerID;
+            //     m_ASLObject.SendAndSetClaim(() => {
+            //         m_ASLObject.SendFloatArray(myFloats);
+            //     });
+            // }
+            // yield return new WaitForSeconds(5);
+            // // go through each playerID in the playerList and remove any players
+            // // that did not return a ping response
+            // foreach(int playerID in originalPlayers)
+            // {
+            //     if(!connectedPlayers.Contains(playerID))
+            //     {
+            //         Debug.Log(GameManager.players[playerID] + " is not connected!");
+            //         // if (GameObject.Find(GameManager.players[playerID]))
+            //         // {
+            //         //     GameObject.Find(GameManager.players[playerID]).transform.parent.gameObject.SetActive(false);
+            //         // }
+            //     }
+            //     if(connectedPlayers.Contains(playerID))
+            //     {
+            //         Debug.Log(GameManager.players[playerID] + " is connected!");
+            //     }
+            // }
+            // connectedPlayers.Clear();
+            GhostPlayer[] ghostList = FindObjectsOfType<GhostPlayer>();
+            foreach(GhostPlayer ghost in ghostList)
             {
-                if (playerID != 1)
+                if (ghost.gameObject.name != GameManager.players[GameManager.MyID])
                 {
-                    myFloats[1] = playerID;
-                    m_ASLObject.SendAndSetClaim(() => {
-                    m_ASLObject.SendFloatArray(myFloats);
-                });
+                    if (!GameLiftManager.GetInstance().m_Players.ContainsValue(ghost.gameObject.name))
+                    {
+                        GameObject.Find(ghost.gameObject.name).transform.parent.gameObject.SetActive(false);
+                    }
                 }
             }
-            yield return new WaitForSeconds(5);
-            // go through each playerID in the playerList and remove any players
-            // that did not return a ping response
-            foreach(int playerID in playerList.Keys)
-            {
-                if (playerID != 1)
-                {
-                    if(!connectedPlayers.Contains(playerID))
-                    {
-                        Debug.Log(GameManager.players[playerID] + " is not connected!");
-                        // if (GameObject.Find(GameManager.players[playerID]))
-                        // {
-                        //     GameObject.Find(GameManager.players[playerID]).transform.parent.gameObject.SetActive(false);
-                        // }
-                    }
-                    if(connectedPlayers.Contains(playerID))
-                    {
-                        Debug.Log(GameManager.players[playerID] + " is connected!");
-                    }
-                }
-            }
-            connectedPlayers.Clear();
         }
     }
 
-    void FloatReceive(string _id, float[] _f)
-    {
-        // if student -> return id as a ping response
-        if((int)_f[0] == PINGREQUEST && (int)_f[1] == GameManager.MyID)
-        {
-            myFloats[0] = PINGRESPONSE;
-            myFloats[1] = GameManager.MyID;
-            m_ASLObject.SendAndSetClaim(() => {
-                m_ASLObject.SendFloatArray(myFloats);
-            });
-        }
+    // void FloatReceive(string _id, float[] _f)
+    // {
+    //     // if student -> return id as a ping response
+    //     if((int)_f[0] == PINGREQUEST && (int)_f[1] == GameManager.MyID)
+    //     {
+    //         myFloats[0] = PINGRESPONSE;
+    //         myFloats[1] = GameManager.MyID;
+    //         m_ASLObject.SendAndSetClaim(() => {
+    //             m_ASLObject.SendFloatArray(myFloats);
+    //         });
+    //     }
 
-        // if teacher -> add student id to connectedPlayers list
-        if((int)_f[0] == PINGRESPONSE && GameManager.AmTeacher)
-        {
-            Debug.Log(GameManager.players[(int)_f[1]] + " has ponged");
-            connectedPlayers.Add((int)_f[1]);
-        }
-    }
+    //     // if teacher -> add student id to connectedPlayers list
+    //     if((int)_f[0] == PINGRESPONSE && GameManager.AmTeacher)
+    //     {
+    //         Debug.Log(GameManager.players[(int)_f[1]] + " has ponged");
+    //         connectedPlayers.Add((int)_f[1]);
+    //     }
+    // }
 }
