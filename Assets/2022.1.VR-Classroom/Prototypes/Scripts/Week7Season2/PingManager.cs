b767254e52060b2b1a41d205eb306bf8e390bfb5
River <riverehill@gmail.com>
2022-07-09 21:01:36 -0700

    Cleaned PingManager.cs

--- Assets/2022.1.VR-Classroom/Prototypes/Scripts/Week7Season2/PingManager.cs
+++ Assets/2022.1.VR-Classroom/Prototypes/Scripts/Week7Season2/PingManager.cs
@@ -28,12 +28,6 @@ public class PingManager : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     IEnumerator SendPing()
     {
         while (true)
