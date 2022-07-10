8fe1fbb4e2cea376c8ce65a77eadb358b39bbc89
River <riverehill@gmail.com>
2022-07-09 19:48:03 -0700

    Add player response to teacher's ping

--- Assets/2022.1.VR-Classroom/Prototypes/Scripts/Week7Season2/PingManager.cs
+++ Assets/2022.1.VR-Classroom/Prototypes/Scripts/Week7Season2/PingManager.cs
@@ -8,15 +8,23 @@ public class PingManager : MonoBehaviour
 
     public float waitTime = 5;
     ASLObject m_ASLObject;
-    float[] id = new float[2];
+    float[] myFloats = new float[2];
+    public Dictionary<int, string> playerList;
+
+    private const float PINGREQUEST = 1000;
+    private const float PINGRESPONSE = 1001;
+
 
     void Start()
     {
         m_ASLObject = GetComponent<ASLObject>();
         m_ASLObject._LocallySetFloatCallback(FloatReceive);
-
+        
         if (GameManager.AmTeacher)
+        {
+            playerList = GameLiftManager.GetInstance().m_Players;
             StartCoroutine(SendPing());
+        }
     }
 
     // Update is called once per frame
@@ -30,18 +38,27 @@ public class PingManager : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(waitTime);
-            id[0] = 100;
+            myFloats[0] = PINGREQUEST;
             m_ASLObject.SendAndSetClaim(() => {
-                m_ASLObject.SendFloatArray(id);
+                m_ASLObject.SendFloatArray(myFloats);
             });
         }
     }
 
     void FloatReceive(string _id, float[] _f)
     {
-        if((int)_f[0] == 100)
+        if((int)_f[0] == PINGREQUEST && !GameManager.AmTeacher)
         {
-            Debug.Log("Recieved Teachers Ping");
+            myFloats[0] = PINGRESPONSE;
+            myFloats[1] = GameManager.MyID;
+            m_ASLObject.SendAndSetClaim(() => {
+                m_ASLObject.SendFloatArray(myFloats);
+            });
+        }
+
+        if((int)_f[0] == PINGRESPONSE && GameManager.AmTeacher)
+        {
+            Debug.Log(GameManager.players[(int)_f[1]] + " has ponged");
         }
     }
 }
