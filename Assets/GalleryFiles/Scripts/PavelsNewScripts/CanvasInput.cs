b5cadd8092cfcbf4cfb45f1fde0b4387fa99c2b3
thesquashedman <76268971+thesquashedman@users.noreply.github.com>
2022-07-25 02:35:37 -0700

    Made sliders work in world space (sort of)
    By doing some amazing click emulation, managed to make sliders actually work

--- Assets/GalleryFiles/Scripts/PavelsNewScripts/CanvasInput.cs
+++ Assets/GalleryFiles/Scripts/PavelsNewScripts/CanvasInput.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.UI;
+using UnityEngine.XR;
+
 public class CanvasInput : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -21,6 +23,10 @@ public class CanvasInput : MonoBehaviour
 
     XRRayInteractor[] VRRaycasters;
 
+    List<InputDevice> leftDevices;
+
+    List<InputDevice> rightDevices;
+
     private void Awake() {
         if (Instance != null && Instance != this) 
         { 
@@ -34,6 +40,13 @@ public class CanvasInput : MonoBehaviour
     void Start()
     {
         StartCoroutine(findPlayer());
+        leftDevices = new List<InputDevice>();
+		var desiredCharacteristicsLeft = UnityEngine.XR.InputDeviceCharacteristics.HeldInHand | UnityEngine.XR.InputDeviceCharacteristics.Left | UnityEngine.XR.InputDeviceCharacteristics.Controller;
+		InputDevices.GetDevicesWithCharacteristics(desiredCharacteristicsLeft, leftDevices);
+
+		rightDevices = new List<InputDevice>();
+		var desiredCharacteristicsRight = UnityEngine.XR.InputDeviceCharacteristics.HeldInHand | UnityEngine.XR.InputDeviceCharacteristics.Right | UnityEngine.XR.InputDeviceCharacteristics.Controller;
+		InputDevices.GetDevicesWithCharacteristics(desiredCharacteristicsRight, rightDevices);
         
     }
 
@@ -47,11 +60,48 @@ public class CanvasInput : MonoBehaviour
             {
                 for(int i = 0; i < VRRaycasters.Length; i++)
                 {
-                    
                     raycastHitObjectVR[i] = VRRaycasters[i].TryGetCurrent3DRaycastHit(out raycastHitsVR[i]);
-                     
+                }
+                //For sliders in world space
+                bool triggerDownLeft;
+                if (leftDevices[0].TryGetFeatureValue(CommonUsages.triggerButton, out triggerDownLeft) && triggerDownLeft)
+                {
+                    RaycastResult result;
+                    if(VRRaycasters[0].TryGetCurrentUIRaycastResult(out result))
+                    {
+                        
+                        //Debug.Log(result.gameObject.name);
+                        if(result.gameObject.name == "Handle")
+                        {
+                            PointerEventData data = new PointerEventData(EventSystem.current);
+                            data.pointerPressRaycast = result;
+                            data.pressPosition = result.screenPosition;
+                            data.position = result.screenPosition;;
+                            result.gameObject.transform.parent.parent.GetComponent<Slider>().OnPointerDown(data);
+                        }
+                    }
+                }
+                bool triggerDownRight;
+		        if (rightDevices[0].TryGetFeatureValue(CommonUsages.triggerButton, out triggerDownRight) && triggerDownRight)
+		        {
+                    RaycastResult result;
+                    if(VRRaycasters[1].TryGetCurrentUIRaycastResult(out result))
+                    {
+                        
+                        //Debug.Log(result.gameObject.name);
+                        if(result.gameObject.name == "Handle")
+                        {
+                            PointerEventData data = new PointerEventData(EventSystem.current);
+                            data.pointerPressRaycast = result;
+                            data.pressPosition = result.screenPosition;
+                            data.position = result.screenPosition;;
+                            result.gameObject.transform.parent.parent.GetComponent<Slider>().OnPointerDown(data);
+                        }
+                    }
                 }
             }
+            
+		
         }
         else{
 
@@ -70,7 +120,7 @@ public class CanvasInput : MonoBehaviour
                 raycastHitObject = Physics.Raycast(ray, out raycastHit, Mathf.Infinity, layerMask);
             }
 
-            
+            //For sliders in world space
             if(Input.GetMouseButton(0))
             {
                 List<RaycastResult> results = new List<RaycastResult>();
@@ -87,12 +137,19 @@ public class CanvasInput : MonoBehaviour
                 {
                     
                     //Debug.Log(result.gameObject.name);
-                    if(result.gameObject.name == "Background")
+                    if(result.gameObject.name == "Handle")
                     {
                         
                         data.pointerPressRaycast = result;
-                        result.gameObject.transform.parent.GetComponent<Slider>().OnPointerDown(data);
+                        result.gameObject.transform.parent.parent.GetComponent<Slider>().OnPointerDown(data);
+                        break;
                         
+                    }
+                    else if(result.gameObject.name == "Background")
+                    {
+                        data.pointerPressRaycast = result;
+                        result.gameObject.transform.parent.GetComponent<Slider>().OnPointerDown(data);
+                        break;
                     }
                     
                 }
