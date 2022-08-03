2cfd353a7f34c73df186b9cfb69a3d27e761bc5b
River <riverehill@gmail.com>
2022-08-02 22:48:13 -0700

    Clean code and add comments

--- Assets/2022.1.VR-Classroom/Prototypes/Scripts/Week7Season2/TutorialScreenController.cs
+++ Assets/2022.1.VR-Classroom/Prototypes/Scripts/Week7Season2/TutorialScreenController.cs
@@ -1,19 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TutorialScreenController : MonoBehaviour
 {
-
     TutorialScreen[] tutorialScreens;
 
     void Start()
     {
+        // find all instances of tutorial screens and store them
         tutorialScreens = GameObject.FindObjectsOfType<TutorialScreen>();
     }
 
     void Update()
     {
+        // toggle screen visibility when 'N' is pressed
         if (Input.GetKeyDown(KeyCode.N))
         {
             ToggleScreens();
@@ -22,6 +21,8 @@ public class TutorialScreenController : MonoBehaviour
 
     void ToggleScreens()
     {
+        // go through each screen and set it's visibility to 
+        // the opposite of it's current visibility
         foreach (TutorialScreen screen in tutorialScreens)
         {
             screen.transform.gameObject.SetActive(!screen.transform.gameObject.activeSelf);
