2894ba907115b47792d1703bbeca3f050e78e83e
River <riverehill@gmail.com>
2022-08-09 08:41:35 -0700

    Update PlayerList
    Added comments, removed old code, and ensured players name would appear at top of list

--- Assets/2022.1.VR-Classroom/Prototypes/Scripts/Week7Season2/General UI/PlayerListScreen.cs
+++ Assets/2022.1.VR-Classroom/Prototypes/Scripts/Week7Season2/General UI/PlayerListScreen.cs
@@ -1,58 +1,37 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using ASL;
 
 public class PlayerListScreen : MonoBehaviour
 {
-    public Dictionary<int, string> playerList;
-    public GameObject List;
-    public GameObject BoothObject;
-    public GameObject TitleObject;
-    public GameObject Player;
-    public bool GenerateType;
-
-    public void generateRangeList()
-    {
-        foreach (Transform child in List.transform)
-        {
-            GameObject.Destroy(child.gameObject);
-        }
-        playerList = GameLiftManager.GetInstance().m_Players;
-        foreach (string item in playerList.Values)
-        {
-            var PlayerName = Instantiate(BoothObject, List.transform, false) as GameObject;
-            PlayerName.GetComponent<SinglelineContainer>().setText(item);
-        }
-    }
+    public Dictionary<int, string> players;
+    public GameObject playerList;
+    public GameObject listItem;
 
     public void generateList()
     {
-        foreach (Transform child in List.transform)
+        // clear list before populating it
+        foreach (Transform child in playerList.transform)
         {
             GameObject.Destroy(child.gameObject);
         }
-        playerList = GameLiftManager.GetInstance().m_Players;
-        foreach (string item in playerList.Values)
+        // populate the playerList entry grid with a list item
+        // for each connected player
+        players = GameLiftManager.GetInstance().m_Players;
+
+        foreach (string playerName in players.Values)
         {
-            var PlayerName = Instantiate(BoothObject, List.transform, false) as GameObject;
-            PlayerName.GetComponent<SinglelineContainer>().setText(item);
+            if (playerName == GameManager.players[GameManager.MyID])
+                continue;
+            AddUser(playerName);
         }
+
+        AddUser(GameManager.players[GameManager.MyID]);
     }
-    
-    //Leaving in incase we want to open it by key press.
-    public void flipScreen()
+
+    void AddUser(string name)
     {
-        if (gameObject.activeSelf == false)
-        {
-            Cursor.lockState = CursorLockMode.Confined;
-            gameObject.SetActive(true);
-            generateList();
-        }
-        else
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            gameObject.SetActive(false);
-        }
+        GameObject newListItem = Instantiate(listItem, playerList.transform, false) as GameObject;
+        newListItem.GetComponent<SinglelineContainer>().setText(name);
     }
 }
